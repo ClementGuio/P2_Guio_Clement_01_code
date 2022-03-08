e12559ba8a083242be685a754510d154c6ab509e
Clément Guio <clementguio@gmail.com>
2022-03-08 17:44:25 +0100

    Ajout des méthodes validKey() et containsKey()

--- rescueHemeBiotech/Project02Eclipse/src/com/hemebiotech/analytics/HashMapSymptoms.java
+++ rescueHemeBiotech/Project02Eclipse/src/com/hemebiotech/analytics/HashMapSymptoms.java
@@ -7,5 +7,26 @@ public class HashMapSymptoms extends HashMap<Symptom, Integer> {
 	public HashMapSymptoms() {
 		super();
 	}
+	
+	@Override
+	public boolean containsKey(Object key){
+		if (!validKey(key)){
+			return false;   //EXCEPTION
+		}
+		Symptom sy = (Symptom) key;
+		for (Symptom s : super.keySet()){
+			if (s.equals(sy)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean validKey(Object obj){
+		if (obj == null) return false;
+		if (obj instanceof Symptom) return true;
+		return false;
+	}
+
 
 }
